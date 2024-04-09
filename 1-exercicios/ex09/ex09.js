bd6d90e5be867e84afae7911cbc3c3dff9da9e5f
Alanis Quintana <alanisquintana.dev@gmail.com>
2024-04-09 13:19:49 -0300

    Exercício 9

---
+++ 1-exercicios/ex09/ex09.js
@@ -0,0 +1,8 @@
+var país = 'Alemanha'
+console.log(`Vivendo em/no/na ${país}`)
+
+if (país == 'Brasileiro') {
+    console.log('Você é brasileiro!')
+} else {
+    console.log('Você é estrangeiro!')
+}
